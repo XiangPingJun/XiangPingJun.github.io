dae48559459890c2d10a04ac58d07b8c46b624f2
Yuan-Hao Lai (RD-TW) <yuanhao_lai@trend.com.tw>
2018-08-17 16:54:28 +0800

    wakfu

--- wakfu-string-search/parser/parseString.js
+++ wakfu-string-search/parser/parseString.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 const lineReader = require('readline').createInterface({
 	input: fs.createReadStream('texts_jp.properties')
 })
-try { fs.unlinkSync('../searcher/texts_zh.js') } catch (e) { }
-const lineWriter = fs.createWriteStream('../searcher/texts_zh.js', { flags: 'a' })
+try { fs.unlinkSync('../texts_zh.js') } catch (e) { }
+const lineWriter = fs.createWriteStream('../texts_zh.js', { flags: 'a' })
 lineWriter.write(`zh={};`)
 const rx = /^([^=]+)=(.*)/
 lineReader.on('line', function (line) {
@@ -19,8 +19,8 @@ lineReader.on('close', () => lineWriter.write(`loaded();`))
 const lineReader2 = require('readline').createInterface({
 	input: fs.createReadStream('texts_en.properties')
 })
-try { fs.unlinkSync('../searcher/texts_en.js') } catch (e) { }
-const lineWriter2 = fs.createWriteStream('../searcher/texts_en.js', { flags: 'a' })
+try { fs.unlinkSync('../texts_en.js') } catch (e) { }
+const lineWriter2 = fs.createWriteStream('../texts_en.js', { flags: 'a' })
 lineWriter2.write(`en={};`)
 lineReader2.on('line', function (line) {
 	line = line.replace(/"/g, '')
