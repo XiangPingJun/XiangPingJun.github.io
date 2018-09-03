435934e4aebccc3d63f1b22d7c42a1e6ea624445
Unknown <shouhei.work@gmail.com>
2018-09-03 19:42:46 +0800

    working

--- gw2/data.js
+++ gw2/data.js
@@ -1,12 +1,16 @@
 const _loc = {
-	'冰龍Jormag': [689, 34, 784, 85],
-	'毒龍Zhaitan(甦醒)': [352, 601, 511, 771],
-	'毒龍Zhaitan(決戰)': [475, 352, 507, 377],
-	'大災變The Cataclysm': [364, 587, 523, 740],
+	'冰龍Jormag': [481, 28, 555, 116],
+	'毒龍Zhaitan(甦醒)': [268, 422, 355, 539],
+	'毒龍Zhaitan(決戰)': [337, 236, 357, 256],
+	'水晶龍Kralkatorrik': [722, 695, 831, 762],
+	'大災變The Cataclysm': [254, 408, 368, 513],
+	'Orr古國': [253, 404, 347, 570]
 }
 const _img = {
 	'jormag.jpg': [559, 150],
-	'Icebrood.jpg': [564, 382]
+	'Icebrood.jpg': [564, 382],
+	'Zhaitan.jpg': [60, 590],
+	'Kralkatorrik.jpg': [60, 590]
 }
 function loc(...args) {
 	return args.map(index => ({ name: index, pos: _loc[index] }))
@@ -20,7 +24,7 @@ const data = [{
 		// '* 火龍Primordus - 1代時被擊敗，第三季的時候似乎有偵測到其活動',
 		//
 	].join('<br/>'),
-	boxes: loc('冰龍Jormag', '毒龍Zhaitan(甦醒)', '大災變The Cataclysm'),
+	boxes: loc('冰龍Jormag', '毒龍Zhaitan(甦醒)', '大災變The Cataclysm', '水晶龍Kralkatorrik'),
 	children: [{
 		caption: '冰龍Jormag',
 		text: [
@@ -34,10 +38,18 @@ const data = [{
 		caption: '毒龍Zhaitan',
 		text: [
 			'毒龍Zhaitan - 個人故事的主線就是要阻止這隻龍。',
-			'他甦醒的時候將被大災變毀滅的Orr古國從海底'
+			'他甦醒的時候將被大災變毀滅的Orr古國從海底，產生的大海嘯淹沒了數千人，甚至淹沒了Lions Arch',
+			'他在遊戲中不斷飛來飛去變換位置與他的爪牙們佔據了很大一塊領地'
 		].join('<br/>'),
 		boxes: loc('毒龍Zhaitan(甦醒)', '毒龍Zhaitan(決戰)'),
-		imgs: [
-		]
+		imgs: img('Zhaitan.jpg')
+	}, {
+		caption: '水晶龍Kralkatorrik',
+		text: [
+			'水晶龍Kralkatorrik - 體型巨大，經過的地方其生物根地貌都會水晶化。飛過之處對大地造成了傷痕，後人稱之為Dragonbrand龍痕。',
+			'傳奇冒險隊 - 命運之刃Destiny\'s Edge 曾經與之戰鬥並造成了Asura英雄Snaff犧牲'
+		].join('<br/>'),
+		boxes: loc('水晶龍Kralkatorrik'),
+		imgs: img('Kralkatorrik.jpg')
 	}],
 }]
