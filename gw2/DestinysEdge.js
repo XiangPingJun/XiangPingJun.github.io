5423e3fe68b08e970d92206f7d182bdc7dccf5c9
Yuan-Hao Lai (RD-TW) <yuanhao_lai@trend.com.tw>
2018-09-05 18:16:17 +0800

    d

---
+++ gw2/DestinysEdge.js
@@ -0,0 +1,42 @@
+const DestinysEdge = {
+	caption: '命運之刃 - 傳奇冒險公會',
+	text: [
+		'命運之刃是泰瑞亞大陸上家喻戶曉的冒險公會, 他們致力於對抗上古巨龍和牠們的爪牙,締造了許多空前的勝利,甚至是直接面對過冰霜巨龍跟水晶巨龍。',
+		'但在最後一場與水晶巨龍Kralkatorrik的戰鬥前, 做為命運之刃強大護盾的重要成員-Logan Thackeray不顧一切的離去前往解救愛慕的珍娜女王Queen Jennah',
+		'不僅導致了行動關鍵的史納夫Snaff死亡,讓整個行動失敗,命運之刃也就此解散。',
+		'命運之刃的成員由各族的英雄組成，GW2遊戲的個人故事就是圍繞著對這些英雄們的憧憬開始。'
+	].join('<br/>'),
+	imgs: img('Destinys_Edge.jpg'),
+	children: [{
+		caption: 'Caithe',
+		text: [
+			'凯西是一名初代sylvari。虽然大部分sylvari 都喜欢阳光，但是凯西从来不畏惧黑暗，只求可以找出真相。作为苍白之树的勇士，凯西的野猎是杀死上古巨龙泽坦。',
+			'当sylvari 还是个年轻种族的时候，大部分的初代sylvari 都希望待在母树身边，但是凯西和法莱恩却想出去看看这个世界。在这段时间，他们建立了深刻的友谊，慢慢的变成了爱人。当 Cadeyrn 将法莱恩带入到梦魇教廷的时候，凯西是同意的。但是当法莱恩最终决定的时候，凯西退缩了，自那以后，两人彻底决裂了。现在，凯西依旧站在梦魇的边缘，虽然拒绝加入法莱恩，但是她们却实质上并没有真正的分开过。'
+		].join('<br/>'),
+	}, {
+		caption: 'Eir Stegalkin & Garm',
+		text: [
+			''
+		].join('<br/>'),
+	}, {
+		caption: 'Logan Thackeray',
+		text: [
+			''
+		].join('<br/>'),
+	}, {
+		caption: 'Rytlock Brimstone',
+		text: [
+			''
+		].join('<br/>'),
+	}, {
+		caption: 'Snaff',
+		text: [
+			''
+		].join('<br/>'),
+	}, {
+		caption: 'Zojja',
+		text: [
+			''
+		].join('<br/>'),
+	}],
+}
