602318cec5e8506534259b350d7f52a2e655bf8d
Unknown <shouhei.work@gmail.com>
2018-09-16 22:46:16 +0800

    a

---
+++ gw2/HumanKingdom.js
@@ -0,0 +1,15 @@
+const HumanKingdom = {
+  caption: '曾經的三大人類王國',
+  text: [
+    '過去的人類曾經受到六真神的庇佑而所向披靡，他們在泰瑞爾大陸上建立了三個王國，分別是Ascalon, Orr跟Tryia。',
+    '很不幸的是由於跟Charr的戰爭以及種種原因，Ascalon跟Orr都已經滅亡，僅存的人類都生活在Tryia'
+  ].join('<br/>'),
+  boxes: loc('阿斯卡隆王國Ascalon', '歐爾王國Orr', '科瑞塔王國Kryta', '阿斯卡隆城', '灼世The Searing', '仇火Foefire'),
+  children: [{
+    caption: 'Ascalon',
+    text: [
+      ''
+    ].join('<br/>'),
+    imgs: img('Caithe.jpg', 'Faolain.jpg')
+  }],
+}
