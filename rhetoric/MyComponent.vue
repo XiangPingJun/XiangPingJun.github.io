4a40c1034c6af51e4671a422264f2a0b09f80a1f
Yuan-Hao Lai (RD-TW) <yuanhao_lai@trendmicro.com>
2020-09-25 19:04:36 +0800

    .

---
+++ rhetoric/MyComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{ hello }}
+  </div>
+</template>
+<style scoped>
+</style>
+<script>
+module.exports = {
+  data: function () {
+    return {
+      hello: "world",
+    };
+  },
+};
+</script>
